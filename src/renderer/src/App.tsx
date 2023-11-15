bd6b5d1ec316bc9c5f8d1677f5235e7ca63b0515
최대열 <dychoi@brainednet.com>
2023-11-15 17:04:25 +0900

    test23223

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainPage from './components/Page/MainPage';
 
-import MainPage from "./components/Page/MainPage";
 
 const App: React.FC = (): JSX.Element => {
   return (
-   <MainPage />
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        {/* 여기에 더 많은 라우트를 추가할 수 있습니다 */}
+      </Routes>
+    </Router>
   );
 };
 
