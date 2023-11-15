7f6557a8dc4d276f510ff0d8e428183fdea699b3
최대열 <dychoi@brainednet.com>
2023-11-15 17:34:50 +0900

    test23223a

--- src/renderer/src/components/Page/MainPage.tsx
+++ src/renderer/src/components/Page/MainPage.tsx
@@ -1,13 +1,15 @@
-import {Button} from "@mui/material";
+import { Button } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
 
 function MainPage(): JSX.Element {
-
+  const navigate = useNavigate();
 
   return (
-   <>
-    <Button>캡처하기</Button>
-   </>
-  )
+    <>
+      <Button onClick={() => navigate('/capture')}>캡처앱으로 이동</Button>
+      <Button onClick={() => navigate('/calculator')}>계산기앱으로 이동</Button>
+    </>
+  );
 }
 
-export default MainPage
+export default MainPage;
